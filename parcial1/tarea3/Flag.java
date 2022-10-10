3c8e1e71cfcb3ffe1e87ee19d477209de8d8907d
TORRESJ11 <jasub1106@gmail.com>
2022-10-10 13:26:41 -0500

    add Operators.java

---
+++ parcial1/tarea3/Flag.java
@@ -0,0 +1,16 @@
+/*
+ * Flag.Java
+ * Author: Jasub
+ * Date: Oct 4, 2022
+ * Location:  Ixtac, Ver. Mexico
+*/
+
+class Flag {  // name of program
+  public static void main(String[] args) {  // main method
+    	
+    boolean flag = true;          
+    System.out.println( "Valor de flag: ");  // define a boolean var 
+    System.out.println(!!flag);      // prints true
+
+  }
+}
